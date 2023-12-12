66985cfec416867f897c36aae291a27c80640ca3
singhsonam2023 <sing.1990sonam@gmail.com>
2023-12-12 15:56:41 +0530

    vnet code

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "azurerm_resource_group" "testrg" {
+  name     = "testrg1"
+  location = "West Europe"
+}
+
+resource "azurerm_network_security_group" "testnsg" {
+  name                = "testnsg1"
+  location            = azurerm_resource_group.testrg.location
+  resource_group_name = azurerm_resource_group.testrg.name
+}
+
+resource "azurerm_virtual_network" "testvn" {
+  name                = "testvn1"
+  location            = azurerm_resource_group.testrg.location
+  resource_group_name = azurerm_resource_group.testrg.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.testnsg.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
